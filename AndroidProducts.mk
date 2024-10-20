bf886a938c98da764043062e82500c6ad0be3a2a
Pabloescobar-reborn <yogivangala07@gmail.com>
2024-10-20 10:12:30 +0000

    test branch

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_marble.mk
+    $(LOCAL_DIR)/voltage_marble.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_marble-user \
-    lineage_marble-userdebug \
-    lineage_marble-eng
+    voltage_marble-user \
+    voltage_marble-userdebug \
+    voltage_marble-eng
---
+++ voltage_marble.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2022-2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from marble device
+$(call inherit-product, device/xiaomi/marble/device.mk)
+
+# Inherit from common CrDroid configuration
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+TARGET_DISABLE_EPPE := true
+TARGET_ENABLE_BLUR := true
+#TARGET_SUPPORTS_64_BIT_APPS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := lineage_marble
+PRODUCT_DEVICE := marble
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := 23049PCD8G
+
+PRODUCT_SYSTEM_NAME := marble_global
+PRODUCT_SYSTEM_DEVICE := marble
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="marble_global-user 14 UKQ1.230804.001 V816.0.8.0.UMRMIXM release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := POCO/marble_global/marble:14/UKQ1.230804.001/V816.0.8.0.UMRMIXM:user/release-keys
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
